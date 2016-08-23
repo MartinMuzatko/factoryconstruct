5c2d7dea4c9db75683f3af6bf138b5e68d35b297
Martin Muzatko <martinm@k15t.com>
2016-08-23 15:22:37 +0200

    Merged changes

--- src/js/engine/factory/factory.js
+++ src/js/engine/factory/factory.js
@@ -2,5 +2,8 @@ export default class Factory {
     constructor(name, resource) {
         this.name = name
         this.resource = resource
+        this.interval = 1
+        this.gain = {resource: resource, amount: 1}
+        this.drain = {resource: resource, amount: 1}
     }
 }
--- src/js/engine/world/world.js
+++ src/js/engine/world/world.js
@@ -1,8 +1,11 @@
 import * as FACTORIES from '../factory/factories'
+import * as RESOURCES from '../resource/resources'
 
 export default class World {
     constructor() {
         this.factories = FACTORIES
+        this.resources = RESOURCES
+        this.tick = 0
     }
 
 }
