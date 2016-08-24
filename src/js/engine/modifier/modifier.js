678c8c97b7d0cd26b1079a4add519c1c44f9492c
Martin Muzatko <martin@happy-css.com>
2016-08-24 19:21:19 +0200

    Added properties to modifier class

--- src/js/engine/modifier/modifier.js
+++ src/js/engine/modifier/modifier.js
@@ -1,6 +1,7 @@
 export default class Modifier {
-    constructor(resource, amount) {
+    constructor(resource = false, amount = 1, interval = 1) {
         this.resource = resource
         this.amount = amount
+        this.interval = interval
     }
 }
