16965d297d08973e728028ca746db832879590f1
Martin Muzatko <martin@happy-css.com>
2016-08-25 00:44:46 +0200

    Document factories and limit workers

--- src/js/engine/factory/factories.js
+++ src/js/engine/factory/factories.js
@@ -3,13 +3,15 @@ import Modifier from '../modifier/modifier'
 import * as RESOURCES from '../resource/resources'
 
 export const IRON_DRILL = new Factory(
-    'Iron Drill',
-    new Modifier(RESOURCES.COAL, 1),
-    new Modifier(RESOURCES.IRON, 5)
+    'Iron Drill', // Name
+    5, // Maximum amount of workers
+    new Modifier(RESOURCES.COAL, 1), // Draining x resources per y ticks
+    new Modifier(RESOURCES.IRON, 5, 5) // Gain x resources per y ticks
 )
 
 export const COAL_DRILL = new Factory(
     'Coal Drill',
+    5,
     new Modifier(),
     new Modifier(RESOURCES.COAL, 2)
 )
--- src/js/engine/factory/factory.js
+++ src/js/engine/factory/factory.js
@@ -1,15 +1,17 @@
 import * as RESOURCES from '../resource/resources'
 
 export default class Factory {
-    constructor(name, drain, gain) {
+    constructor(name, workerLimit = 5, drain, gain) {
         this.name = name
         this.drain = drain
         this.gain = gain
         this.worker = []
+        this.workerLimit = workerLimit
     }
 
     addWorker() {
-        if (RESOURCES.WORKER.consume(1)) {
+        if (RESOURCES.WORKER.canConsume(1) && this.worker.length < this.workerLimit) {
+            RESOURCES.WORKER.consume(1)
             this.worker.push(0)
         }
     }
