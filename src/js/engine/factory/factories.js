987f9257731c614fd6bac76541bff62c19df2956
Martin Muzatko <martin@happy-css.com>
2016-08-25 01:00:17 +0200

    Add various resources and factories

--- src/js/engine/factory/factories.js
+++ src/js/engine/factory/factories.js
@@ -2,6 +2,13 @@ import Factory from './factory'
 import Modifier from '../modifier/modifier'
 import * as RESOURCES from '../resource/resources'
 
+export const COAL_MINE = new Factory(
+    'Coal Drill',
+    5,
+    new Modifier(),
+    new Modifier(RESOURCES.COAL, 2)
+)
+
 export const IRON_DRILL = new Factory(
     'Iron Drill', // Name
     5, // Maximum amount of workers
@@ -9,9 +16,16 @@ export const IRON_DRILL = new Factory(
     new Modifier(RESOURCES.IRON, 5, 5) // Gain x resources per y ticks
 )
 
-export const COAL_DRILL = new Factory(
-    'Coal Drill',
+export const COPPER_DRILL = new Factory(
+    'Copper Drill',
     5,
-    new Modifier(),
-    new Modifier(RESOURCES.COAL, 2)
+    new Modifier(RESOURCES.COAL, 1),
+    new Modifier(RESOURCES.COPPER, 3, 6)
+)
+
+export const WIRE_EXTRUDER = new Factory(
+    'Wire Extruder',
+    5,
+    new Modifier(RESOURCES.COPPER, 1, 5),
+    new Modifier(RESOURCES.WIRE, 2)
 )
--- src/js/engine/resource/resources.js
+++ src/js/engine/resource/resources.js
@@ -1,5 +1,8 @@
 import Resource from './resource'
 
-export const IRON = new Resource('Iron')
 export const COAL = new Resource('Coal')
+export const IRON = new Resource('Iron')
+export const COPPER = new Resource('Copper')
+export const WIRE = new Resource('Wire')
+export const COMPUTER_CHIP = new Resource('Computer Chip')
 export const WORKER = new Resource('Worker', 10)
