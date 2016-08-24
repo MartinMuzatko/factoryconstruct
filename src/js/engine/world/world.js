ae93b0734a9a8f68ad98f60e456c56f797b52341
Martin Muzatko <martin@happy-css.com>
2016-08-24 19:21:53 +0200

    Move resource calculations to world

--- src/js/engine/world/world.js
+++ src/js/engine/world/world.js
@@ -1,11 +1,32 @@
 import * as FACTORIES from '../factory/factories'
 import * as RESOURCES from '../resource/resources'
+import Resource from '../resource/resource'
+import Modifier from '../modifier/modifier'
 
 export default class World {
-    constructor() {
+    constructor(appContext) {
         this.factories = FACTORIES
         this.resources = RESOURCES
         this.tick = 0
+        this.tickUnit = 1000
+        this.app = appContext
+        this.startTick()
+    }
+
+    startTick() {
+        setInterval(() => {
+            this.handleProduction()
+            this.app.update()
+        }, this.tickUnit)
+    }
+
+    handleProduction() {
+        for (let factory in this.factories) {
+            factory = this.factories[factory]
+            if (factory.gain instanceof Modifier) {
+                factory.gain.resource.amount += factory.gain.amount
+            }
+        }
     }
 
 }
